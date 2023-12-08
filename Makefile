bb1b244ab9969e2b4654e0b5ce367689e644b18f
Momen-Assaf <momenssf@gmail.com>
2023-12-08 15:48:53 +0200

    init

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC = gcc
-CFLAGS = -Wall -Werror
+CFLAGS = -Wall
 
 all: parent customer cashier 
 	./parent
--- parent.c
+++ parent.c
@@ -2,5 +2,13 @@
 
 int main() {
     printf("Hello, World!\n");
+
+    FILE *items = fopen("items","r");
+    if(items == NULL ){
+        perror("Error opening Items file");
+        return -1;
+    }
+    fclose(items);
+
     return 0;
 }
