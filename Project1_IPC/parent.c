54e8af570e272d9d2712a05c7b0f620f612ff333
Momen-Assaf <momenssf@gmail.com>
2023-12-09 08:54:07 +0200

    patch 1.0 customer

--- Project1_IPC/parent.c
+++ Project1_IPC/parent.c
@@ -1,69 +1,147 @@
 #include "header.h"
 
-void kill_sharedmem(int shmid ,char *shm){
-     // Detach the shared memory segment
-    if (shmdt(shm) == -1) {
+void endRun(int shmid, char *shm, pid_t pid)
+{
+    // Detach the shared memory segment
+    if (shmdt(shm) == -1)
+    {
         perror("shmdt");
         exit(EXIT_FAILURE);
     }
 
     // Delete the shared memory segment
-    if (shmctl(shmid, IPC_RMID, NULL) == -1) {
+    if (shmctl(shmid, IPC_RMID, NULL) == -1)
+    {
         perror("shmctl");
         exit(EXIT_FAILURE);
     }
+    // Terminate the child process using SIGTERM
+    if (kill(pid, SIGTERM) == -1)
+    {
+        perror("kill");
+        exit(EXIT_FAILURE);
+    }
 }
-int main() {
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+
     pid_t pid;
     int shmid;
-    char *shm, *s;
+    char *s;
 
-    FILE *items = fopen("items","r");
-    if(items == NULL ){
+    FILE *items = fopen(argv[1], "r");
+    if (items == NULL)
+    {
         perror("Error opening Items file");
         return -1;
     }
 
+    readUserDefined();
+
     // Skip the first line
     char line[256];
-    if (fgets(line, sizeof(line), items) == NULL) {
+    if (fgets(line, sizeof(line), items) == NULL)
+    {
         perror("Error reading file");
         fclose(items);
-        return 1;
+        return -2;
     }
 
     // Create shared memory segment
-    if ((shmid = shmget(IPC_PRIVATE, SHM_SIZE, IPC_CREAT | 0666)) < 0) {
+    if ((shmid = shmget(IPC_PRIVATE, SHM_SIZE, IPC_CREAT | 0666)) < 0)
+    {
         perror("shmget fail");
-        exit(3);
+        exit(-3);
     }
 
     // Attach shared memory segment to the process address space
-    if ((shm = (char *)shmat(shmid, 0, 0)) == (char *)-1) {
+    if ((shm = (char *)shmat(shmid, 0, 0)) == (char *)-1)
+    {
         perror("shmat: parent");
-        exit(4);
+        exit(-4);
     }
 
     char fileData[SHM_SIZE];
 
     // Read and discard the first line
-    if (fgets(shm, SHM_SIZE, items) == NULL) {
+    if (fgets(shm, SHM_SIZE, items) == NULL)
+    {
         perror("Error reading file");
         fclose(items);
-        kill_sharedmem(shmid,shm);
-        return 1;
+        endRun(shmid, shm, pid);
+        return -5;
     }
 
     // Read and accumulate the rest of the file into shared memory
     while (fgets(shm + strlen(shm), SHM_SIZE - strlen(shm), items) != NULL);
 
-    printf("All Data:\n%s", shm);
+    printf("Products in Store:\n");
+    displayProductData(shm);
 
     close(items);
+    pid = fork();
 
-    sleep(1);
+    switch (pid)
+    {
+    case -1:
+        perror("fork");
+        exit(-6);
+        break;
 
-    kill_sharedmem(shmid,shm);
+    case 0: // CUSTOMER
+
+        srand((unsigned int)getpid());
+
+        char *token = strtok(shm, "\n");
+        char modifiedData[SHM_SIZE];
+        int offset = 0;
+
+        while (token != NULL)
+        {
+            int quantity, price;
+            char product[20];
+            sscanf(token, "%s %d %d", product, &quantity, &price);
+
+            // Remove random quantity between 0 and 5
+            int quantityToRemove = rand() % 4; 
+            if (quantityToRemove > 0)
+            {
+                printf("\nChild removing %d quantity from %s\n", quantityToRemove, product);
+
+                int quantityAfterRemoval = quantity - quantityToRemove;
+                if (quantityAfterRemoval < 0)
+                {
+                    quantityAfterRemoval = 0;
+                }
+
+                // Update the data in modified buffer
+                offset += snprintf(modifiedData + offset, SHM_SIZE - offset, "%s %d %d\n", product, quantityAfterRemoval, price);
+
+                // Additional print statement to trace modification
+                printf("Child modified: %s %d %d\n", product, quantityAfterRemoval, price);
+            }
+            token = strtok(NULL, "\n");
+        }
+
+        // Copy the modified data back to shared memory
+        strcpy(shm, modifiedData);
+        return 0;
+
+    default:
+        sleep(2);
+
+        // Display product data after child modification
+        printf("Remaining Products in Store:\n");
+        displayProductData(shm);
+
+        endRun(shmid, shm, pid);
+        exit(0);
+    }
 
     return 0;
 }
