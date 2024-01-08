874b9a016317c51434e858b3654ad05c85ab85a6
Momen-Assaf <momenssf@gmail.com>
2024-01-08 19:27:48 +0200

    userdefined

--- Project2_Threads/header.h
+++ Project2_Threads/header.h
@@ -18,4 +18,20 @@
 #include <math.h>
 #include <stdbool.h>
 
-pid_t ogl_id , shelvteam_id[3];
+pid_t ogl_id;
+
+void readUserDefined(int *Shelving_Teams, int *Employees_Number, int *Shelf_drop_Threshold, int *Simulation_Time_Threshold)
+{
+    FILE *file = fopen("userDefined.txt", "r");
+    if (file == NULL)
+    {
+        printf("Error opening the file.\n");
+        return;
+    }
+    fscanf(file, "Shelving_Teams: %d\n", Shelving_Teams);
+    fscanf(file, "Employees_Number: %d\n", Employees_Number);
+    fscanf(file, "Shelf_drop_Threshold: %d\n", Shelf_drop_Threshold);
+    fscanf(file, "Simulation_Time_Threshold: %d\n", Simulation_Time_Threshold);
+
+    fclose(file);
+}
--- Project2_Threads/parent.c
+++ Project2_Threads/parent.c
@@ -2,50 +2,61 @@
 
 int main(int argc, char *argv[])
 {
+    int Shelving_Teams = 0;
+    int Employees_Number = 0;
+    int Shelf_drop_Threshold = 0;
+    int Simulation_Time_Threshold = 0;
+    readUserDefined(&Shelving_Teams, &Employees_Number, &Shelf_drop_Threshold, &Simulation_Time_Threshold);
+
     ogl_id = fork();
-    
-    if(ogl_id < 0){
+
+    if (ogl_id < 0)
+    {
         perror("Error creating ogl process:");
         exit(EXIT_FAILURE);
-        
     }
-    else if( ogl_id == 0 ){
-        execlp("./ogl", "OPENGL" , NULL);
+    else if (ogl_id == 0)
+    {
+        execlp("./ogl", "OPENGL", NULL);
         perror("Error opening ogl process:");
         exit(EXIT_FAILURE);
     }
+    pid_t shelvteam_id[Shelving_Teams];
 
-    for( int i = 0; i < 3; i++){
+    for (int i = 0; i < Shelving_Teams; i++)
+    {
         shelvteam_id[i] = fork();
-    
-        if(shelvteam_id[i] < 0){
+
+        if (shelvteam_id[i] < 0)
+        {
             perror("Error creating shelving team process:");
             exit(EXIT_FAILURE);
-            
         }
-        else if( shelvteam_id[i] == 0 ){
-            execlp("./shelvingTeam", "SHELVING TEAM" , NULL);
+        else if (shelvteam_id[i] == 0)
+        {
+            execlp("./shelvingTeam", "SHELVING TEAM", NULL);
             perror("Error opening shelving team process:");
             exit(EXIT_FAILURE);
         }
     }
 
-    while(1){
+    while (1)
+    {
         sleep(2);
         pid_t customer_id = fork();
-    
-        if(customer_id < 0){
+
+        if (customer_id < 0)
+        {
             perror("Error creating customer process:");
             exit(EXIT_FAILURE);
-            
         }
-        else if( customer_id == 0 ){
-            execlp("./customer", "CUSTOMER" , NULL);
+        else if (customer_id == 0)
+        {
+            execlp("./customer", "CUSTOMER", NULL);
             perror("Error opening customer process:");
             exit(EXIT_FAILURE);
         }
         break;
     }
     exit(0);
-
 }
